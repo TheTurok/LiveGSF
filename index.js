c1624803248199ce72339959a290998a60533716
Timothy Choi <Timothy.Choi@wdc.com>
2019-12-13 15:48:10 -0800

    ?

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app); //passing in app into auth routes!
 
 
-if (process.env.NODE_ENV == 'production'){
+if (process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
   const path require('path');
   app.get('*', (req,res) => {
