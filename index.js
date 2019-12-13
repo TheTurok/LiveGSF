68136acb990426dd49d923752d1b067efd3fbb1d
Timothy Choi <Timothy.Choi@wdc.com>
2019-12-12 16:39:38 -0800

    added to heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require('express');  // express helps us create and server app!
 const app = express();
 
 
-app.get('/', (req,res) => {
-  res.send({hi:'there'});
+app.get('/', (req,res) => {  // at main url  -- / receive request
+  res.send({hi:'there'});  // result sending to this location of url
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT);
