7eb8abe3a656fe7a5a4914c9303ac91c56398ef1
Timothy Choi <Timothy.Choi@wdc.com>
2019-12-13 15:41:11 -0800

    small udpate

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app); //passing in app into auth routes!
 
 
-if process.env.NODE_ENV == 'production'){
+if (process.env.NODE_ENV == 'production'){
   app.use(express.static('client/build'));
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
