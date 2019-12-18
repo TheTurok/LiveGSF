ff53ecc34aee7d06949b423f612bc21af8d6aeab
[Kenneth Vu] <[Kenneth.Vu@wdc.com]>
2019-12-18 12:51:52 -0800

    merging

--- client/src/components/current_measurement/CurrentMeasurement.js
+++ client/src/components/current_measurement/CurrentMeasurement.js
@@ -4,8 +4,11 @@ class CurrentMeasurement extends Component{
   render(){
     return(
       <div >
-        <h1> CurrentMeasurement </h1>
+        <h1> Current Measurement </h1>
       </div>
+      //<li>
+        //<status status = {'idle'} />
+      //</li>
     );
   }
 };
