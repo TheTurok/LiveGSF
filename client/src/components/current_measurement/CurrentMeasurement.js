259b6708e03c82be342cfaa873fed5ec2df7ef09
[Kenneth Vu] <[Kenneth.Vu@wdc.com]>
2019-12-19 09:44:54 -0800

    stuff

--- client/src/components/current_measurement/CurrentMeasurement.js
+++ client/src/components/current_measurement/CurrentMeasurement.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import MeasurementStatus from './MeasurementStatus'
 
 class CurrentMeasurement extends Component{
   render(){
     return(
       <div >
-        <h1> CurrentMeasurement </h1>
+        <h1> Current Measurement </h1>
+        <MeasurementStatus />
       </div>
     );
   }
---
+++ client/src/components/current_measurement/MeasurementStatus.js
@@ -0,0 +1,39 @@
+// Shows the measurement status, red, yellow, green
+import React, {Component} from 'react';
+import { reduxForm } from 'redux-form';
+
+class MeasurementStatus extends Component {
+  render() {
+    return (
+      <div>
+        poopydoop
+      </div>
+    );
+    /*
+    switch (status) {
+      case 'on':
+        return (
+          <div>
+            GSF status: {this.props.status}
+          </div>
+        );
+        break;
+      case 'error':
+        return (
+          <div>
+            GSF status: {this.props.status}
+          </div>
+        );
+        break;
+      default:
+        return (
+          <div>
+            GSF status: {this.props.status}
+          </div>
+        );
+        break;
+    */
+  }
+};
+
+export default MeasurementStatus;
