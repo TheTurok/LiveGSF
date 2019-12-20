e864e6d4f58c9fa5b2e4c35f9422c62ac4565370
[Kenneth Vu] <[Kenneth.Vu@wdc.com]>
2019-12-20 14:07:17 -0800

    track history working

--- client/src/components/tracker/TrackerList.js
+++ client/src/components/tracker/TrackerList.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchTracker } from '../../actions';
 import _ from 'lodash';
+import {WAFERS} from '../current_measurement/formFields';
+import { renderTrayFields } from '../current_measurement/CurrentMeasurement';
 
 class TrackerList extends Component {
   componentDidMount() { //call action creator fetchTracker upon mount
     this.props.fetchTracker();
+  }
+
+  renderTrayFields(trays){
+    if(trays) {
+      return _.map(trays, ({wafer, quantity, bin}) => {
+        if (wafer){
+          return(
+            <div style={{margin: '5px'}} key={wafer} className="wafer-information">
+              <div className="white-text">{wafer}</div>
+              <div className="white-text">{quantity}</div>
+              <div className="white-text">{bin}</div>
+            </div>
+          );
+        }
+      });
+    }
   }
 
   renderTrackerList() {
@@ -15,18 +33,19 @@ class TrackerList extends Component {
           <div className="card darken-1" key={aSingleTrack._id}>
             <div className="card-content">
               <span className="card-title">{aSingleTrack.title}</span>
+              {this.renderTrayFields(aSingleTrack.trays)}
               <p>
-                {aSingleTrack.body}
+                {aSingleTrack.notes}
               </p>
-              <p className="right">
-                Start Date: {new Date(aSingleTrack.start).toLocaleDateString()}
+              <p>
+                {aSingleTrack.link}
               </p>
               <p className="right">
                 End Date: {new Date(aSingleTrack.end).toLocaleDateString()}
               </p>
-            </div>
-            <div className="card-action">
-
+              <p className="right">
+                Start Date: {new Date(aSingleTrack.start).toLocaleDateString()}
+              </p>
             </div>
           </div>
         );
